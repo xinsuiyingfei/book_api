4c0932a5a4a2f64050bf1b927099dc5328054062
zhaowen <2308365523@qq.com>
2021-11-13 15:58:44 +0800

    添加参数化

--- conftest.py
+++ conftest.py
@@ -12,7 +12,7 @@ from utils.interface import http
 
 
 
-# def pytest_addoption(parser):
+# def pytest_addoption(parser):    #pytest 运行时参数
 #     parser.addoption(
 #         "--setting",
 #         action="store",
@@ -23,12 +23,13 @@ from utils.interface import http
 
 @pytest.fixture(scope='session', autouse=True)
 def before_after(request):  # 前置条件和后置条件
-
+    # http.set(request.config.option.setting)   #初始化环境信息   方法一，用pytest 的config
+    http.set(sys.argv[1])   #方法二 利用python 自带的sys.argv 
     logger_cls.info("服务级-前置条件")
-    http.set()   #初始化环境信息
     http.login() #登录，不传用户名密码，则使用配置文件里的用户
     yield before_after  # 后置条件
     logger_cls.info("服务级-后置条件")
+
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import  pytest
 
-pytest.main(['-s','test_book','--alluredir','./report/'])
+pytest.main(['-s','test_book','--alluredir','./report/'])   
 # pytest.main(['-s','test_book','-k','book','--alluredir','./report/'])   #执行所有包含book的文件
+# pytest -s  test_book --alluredir ./report/ --setting test2    
