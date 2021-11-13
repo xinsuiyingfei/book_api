5e70b699404fd1cd766fe6a4a2339a6fe9ff30b8
Kevin.zhao <48636858+zhaowen13@users.noreply.github.com>
2021-11-13 20:20:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import  pytest
 
 pytest.main(['-s','test_book','--alluredir','./report/'])   
 # pytest.main(['-s','test_book','-k','book','--alluredir','./report/'])   #执行所有包含book的文件
-# pytest -s  test_book --alluredir ./report/ --setting test2    
+# pytest -s  test_book --alluredir ./report/ --setting test2    1111111111
